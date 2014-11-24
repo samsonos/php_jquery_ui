5b4a431f7b0be98722a52ae975b6cda95a2ad06a
onysko <pavlo.onysko@gmail.com>
2014-11-24 16:48:27 +0200

    module created

---
+++ samson.php
@@ -0,0 +1,16 @@
+<?php
+namespace samson\jquery;
+
+/**
+ * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ *
+ * @package SamsonPHP
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ * @author Nikita Kotenko <nick.w2r@gmail.com>
+ * @version 0.1
+ */
+
+class JQueryConnector extends \samson\core\CompressableExternalModule
+{
+	protected $id = 'jquery'; 
+}
